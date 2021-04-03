290af9c6d250384dfb3e03b2ff6270e8af29c8d4
AgostinaArevaloJaube <agosarevalo@gmail.com>
2021-04-03 00:50:02 -0300

    Add displayCharacterSection

---
+++ src/layout/displayCharacterSection.js
@@ -0,0 +1,28 @@
+export function displayCharacterSection(section, character) {
+	const characterContainer = document.createElement('div');
+	characterContainer.innerHTML = `
+	<div class="character-section">
+		<div class="character-description">
+			<div class="character-text-container">
+				<h3 class="character-name">${character.name}</h3>
+				<i class="far fa-heart icons"></i>
+			</div>
+			<p>
+				${
+					character.description === ''
+						? 'We are creating this character. No description available yet. We promise that we are working very hard to achieve it.'
+						: character.description
+				}
+			</p>
+		</div>
+
+		<img
+			class="character-photo"
+			src=${character.thumbnail.path}.${character.thumbnail.extension}
+			alt=${character.name}
+		/>
+	</div>
+	<a href="index.html" class="regular-button back-home-btn">Back home</a>
+	`;
+	section.appendChild(characterContainer);
+}
