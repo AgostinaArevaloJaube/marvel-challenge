1832f3884b93938b839dd4bd1ad8497a8e1f7d20
AgostinaArevaloJaube <agosarevalo@gmail.com>
2021-04-03 01:12:51 -0300

    Get dom elements

--- src/handlers/domElements.js
+++ src/handlers/domElements.js
@@ -1,5 +1,7 @@
-export const $allSuperheroesSection = document.querySelector(
+
+export const titleSection = document.querySelector('#title-section')
+export const allSuperheroesSection = document.querySelector(
 	'#all-superheroes'
 );
-export const $favoriteSection = document.querySelector('#favorites-section');
-export const $characterSection = document.querySelector('#character-section')
+export const favoriteSection = document.querySelector('#favorites-section');
+export const characterSection = document.querySelector('#character-section')
