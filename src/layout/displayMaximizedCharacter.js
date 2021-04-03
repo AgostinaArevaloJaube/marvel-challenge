800afe5c917f20bcf8cda4d7fef7792e963c2938
AgostinaArevaloJaube <agosarevalo@gmail.com>
2021-04-03 01:10:42 -0300

    Add displayMaximizedCharacter

---
+++ src/layout/displayMaximizedCharacter.js
@@ -0,0 +1,24 @@
+import { getCharacterById } from '../services/api.js';
+import { hideSection, displaySection } from '../handlers/handleSections.js';
+import {
+	titleSection,
+	allSuperheroesSection,
+	favoriteSection,
+	characterSection,
+} from '../handlers/domElements.js';
+import {
+	activateSpinner,
+	desactivateSpinner,
+} from '../handlers/handleSpinner.js';
+import { displayCharacterSection } from '../layout/displayCharacterSection.js';
+
+export async function displayMaximizedCharacter(id) {
+	hideSection(allSuperheroesSection);
+	hideSection(favoriteSection);
+	hideSection(titleSection);
+	activateSpinner();
+	displaySection(characterSection, 'character-container');
+	const character = await getCharacterById(id);
+	desactivateSpinner();
+	displayCharacterSection(characterSection, character);
+}
