864d2142ac3ee61f9c634fcaad56227208429ed0
AgostinaArevaloJaube <agosarevalo@gmail.com>
2021-04-03 01:12:28 -0300

    Add function to hide and display sections

--- src/layout/displayAllCharacters.js
+++ src/layout/displayAllCharacters.js
@@ -4,13 +4,23 @@ import {
 	desactivateSpinner,
 } from '../handlers/handleSpinner.js';
 import { displayCharacter } from '../layout/displayCharacter.js';
-import { $allSuperheroesSection } from '../handlers/domElements.js';
+import { allSuperheroesSection } from '../handlers/domElements.js';
+import { hideSection, displaySection } from '../handlers/handleSections.js';
+import {
+	titleSection,
+	favoriteSection,
+	characterSection,
+} from '../handlers/domElements.js';
 
 export async function displayAllCharacters() {
 	activateSpinner();
+	hideSection(titleSection);
+	hideSection(favoriteSection);
+	hideSection(characterSection);
 	const allCharacters = await getAllCharacters();
 	allCharacters.map((character) =>
-		displayCharacter($allSuperheroesSection, character)
+		displayCharacter(allSuperheroesSection, character)
 	);
+	displaySection(titleSection, 'superheroes-section');
 	desactivateSpinner();
 }
