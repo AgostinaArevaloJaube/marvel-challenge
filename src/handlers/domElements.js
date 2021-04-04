da80216335bf7e726be1440e1a9688c5a179e448
AgostinaArevaloJaube <agosarevalo@gmail.com>
2021-04-03 22:28:11 -0300

    Add dom elements

--- src/handlers/domElements.js
+++ src/handlers/domElements.js
@@ -1,5 +1,14 @@
 export const titleSection = document.querySelector('#title-section');
 export const allSuperheroesSection = document.querySelector('#all-superheroes');
+export const hero = document.querySelector('#hero');
 export const favoriteSection = document.querySelector('#favorites-section');
+export const favoritePage = document.querySelector('#favorites-page');
+export const favoriteContainer = document.querySelector(
+	'#favorites-container'
+);
+
 export const characterSection = document.querySelector('#character-section');
 export const errorContainer = document.querySelector('#error-container');
+
+export const searchInput = document.querySelector('#search-input');
+export const searchButton = document.querySelector('#search-button');
