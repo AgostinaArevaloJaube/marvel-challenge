d4591b766e5086e80d90af7b1fb0284bc3c6722c
AgostinaArevaloJaube <agosarevalo@gmail.com>
2021-04-04 15:30:52 -0300

    Try new version of character card

--- src/components/displayCharacterCard.js
+++ src/components/displayCharacterCard.js
@@ -13,16 +13,19 @@ export function displayCharacterCard(container, character) {
 		onclick="displayMaximizedCharacter(${character.id})"
 	>
 	<div class="character-info">
-		<p class="character-name" onclick="displayMaximizedCharacter(${
-			character.id
-		})">${character.name}</p>
-		${
-			arrFavoritesCharacters
-				? `<i class="far fa-trash-alt icons" onclick="removeFromFav('${character.id}')"></i>`
-				: `<i class="far fa-heart icons" onclick="addToFav('${character.name}', '${character.id}', '${character.thumbnail.path}','${character.thumbnail.extension}')"></i>`
-		}
+		<p class="character-name" onclick="displayMaximizedCharacter(${character.id})">${character.name}</p>
+
 
 	</div>`;
 
 	container.appendChild(characterContainer);
 }
+
+/*
+		//  ${
+		// 	arrFavoritesCharacters
+		// 		? `<i class="far fa-trash-alt icons" onclick="removeFromFav('${character.id}')"></i>`
+		// 		: `<i class="far fa-heart icons" onclick="addToFav('${character.name}', '${character.id}', '${character.thumbnail.path}','${character.thumbnail.extension}')"></i>`
+		//  }
+
+	*/
